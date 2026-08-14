06f01a343eafa8e0a129e523fbafbb40ca82af42
Your Name <itismepranish@gmail.com>
2026-08-14 12:37:55 +0545

    chore: Added Artificial Delay

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -17,6 +17,7 @@ public class ProductController(
         [FromQuery] int pageSize = 4
         )
     {
+        await Task.Delay(2000);
         return Ok(await productService.GetProducts(category, page, pageSize));
     }
 
